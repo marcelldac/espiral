0f559fb3691d87565c62a2e776b37aaf37509a53
Marcell Dactes <83461178+marcelldac@users.noreply.github.com>
2022-08-16 12:24:54 -0300

    Add files via upload

---
+++ espiral.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+#rainbow = ['red','green','blue']
+#rainbow_var = 'red','green','blue'
+
+speed(0)
+bgcolor('black')
+color('green', 'black')
+
+for i in range(900):
+    fd(i*2/2+1)
+    lt(360/2+1.50)
+
+done()
